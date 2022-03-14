a93b0a76d0a09058a8af4c2acf30eebe855405aa
Dan Jakob Ofer <dan@ofer.to>
2022-03-14 16:09:23 -0400

    It works

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import TurndownService from "turndown";
 import { join } from "path";
 import { JSDOM } from "jsdom";
-import { Dir, opendirSync, readFileSync, Dirent } from "fs";
+import { snakeCase, last, toNumber } from "lodash";
+import { opendirSync, writeFileSync, readFileSync, Dirent, existsSync, mkdirSync, appendFileSync } from "fs";
 
-class Html {
+class Markdown {
   readonly dom: JSDOM;
 
   get document(): Document {
@@ -14,12 +15,18 @@ class Html {
     return (this.document.querySelector("#title-text > a") as HTMLAnchorElement)?.textContent || "";
   }
 
-  get htmlInformation(): string {
-    return (this.document.querySelector("#content") as HTMLDivElement)?.innerHTML || "";
+  get body(): string {
+    return (this.document.querySelector("#content > #main-content") as HTMLDivElement)?.innerHTML || "";
   }
 
   constructor(raw: string) {
     this.dom = new JSDOM(raw);
+  }
+
+  toString(): string {
+    let markdown = service.turndown(this.body);
+
+    return `# ${this.title}\n\n${markdown}`;
   }
 }
 
@@ -33,22 +40,100 @@ let readHtmlFiles = (directory: string): string[] => {
     let path = join(directory, file.name);
     console.log(path);
 
-    let result = readFileSync(`${directory}/1.html`).toString();
-    results.push(result);
+    try {
+      let result = readFileSync(path).toString();
+      results.push(result);
+    } catch (error) {
+      console.error(`Failed to read ${path}`);
+    }
   }
 
   return results;
-}
+};
+
+let writeMarkdownFile = (directory: string, input: Markdown): void => {
+  let filename = `${snakeCase(input.title)}.md`;
+  let path = join(directory, filename);
+
+  console.log(`Creating ${path}`);
+
+  if (!existsSync(directory)) {
+    try {
+      mkdirSync(directory, { recursive: true });
+    } catch (error) {
+      console.error(`Failed to create ${directory}`);
+      process.exit(1);
+    }
+  }
+
+  try {
+    writeFileSync(path, input.toString());
+
+    console.log("Success");
+  } catch (error) {
+    console.error(`Failed to create ${path}`);
+    console.error("Continuing to next markdown file");
+  }
+};
+
+let getCurrentExecution = (directory: string): number => {
+  if (!existsSync(directory)) {
+    try {
+      mkdirSync(directory, { recursive: true });
+    } catch (error) {
+      console.error(`Failed to create ${directory}`);
+      process.exit(1);
+    }
+  }
+
+  const filename = "executions.txt";
+  const filepath = join(directory, filename);
+  if (!existsSync(filepath)) {
+    try {
+      writeFileSync(filepath, "0");
+    } catch (error) {
+      console.error(`Unable to create ${filepath}`);
+      process.exit(1);
+    }
+  }
+
+  let data: string;
+  try {
+    data = readFileSync(filepath).toString();
+  } catch (error) {
+    console.error("File does not exist");
+    process.exit(1);
+  }
+
+  let executions = data.split("\n");
+  let current = toNumber(last(executions)) + 1;
+  try {
+    appendFileSync(filepath, `\n${current}`);
+  } catch (error) {
+    console.error(`Failed to write to ${filepath}`);
+    process.exit(1);
+  }
+
+  console.log(`Current Execution is: ${current}`);
+
+  return current;
+};
 
 let service = new TurndownService({
   headingStyle: "atx",
   bulletListMarker: "-",
   codeBlockStyle: "fenced",
 });
-readHtmlFiles("html").forEach(item => {
-  let html = new Html(item);
-  let markdown = service.turndown(html.htmlInformation);
 
-  console.log(html.title);
-  console.log(markdown);
+const execution = getCurrentExecution("markdown");
+const markdownDirectory = join("markdown", execution.toString());
+
+const htmlDirectory = "html";
+
+console.log(`Reading html files at "${htmlDirectory}"`);
+readHtmlFiles(htmlDirectory).forEach(item => {
+  console.log();
+
+  let markdown = new Markdown(item);
+  writeMarkdownFile(markdownDirectory, markdown);
 });
